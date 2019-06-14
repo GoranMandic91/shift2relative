5a6ad0e2aba03c0007d3197539d8986d370f1dcd
Goran Mandic <godzi1991@gmail.com>
2019-06-14 15:48:09 +0200

    Update README file

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # shift2relative
-Easy migrate to relative require paths in JavaScript
+[![npm version](https://badge.fury.io/js/shift2relative.svg)](https://badge.fury.io/js/shift2relative)
+
+Easy shift to relative require paths in JavaScript
+
+# How to run 
+
+```
+npx shift2relative [OPTIONS]
+```
+
+Currently supported options:
+   - `--root`: path to root directory, if not present defaults to `/`
+   - `--dry`: enables run in test mode, defaults to `false`
--- src/index.js
+++ src/index.js
@@ -6,26 +6,25 @@ const minimist = require('minimist');
 const logger = require('./logger');
 const calculate = require('./calculate');
 
-const { root, dryRun } = minimist(process.argv.slice(2));
+const { root = '/', dry = false } = minimist(process.argv.slice(2));
 
 function ignoreFile(file) {
   return path.extname(file) !== '' && path.extname(file) !== '.js';
 }
 
-if (!root) {
-  logger.logError({ error: 'Need to specify root directory! Example: npx shift2relative --root=path' });
-} else {
-  const directoryPath = process.cwd() + root;
+const directoryPath = process.cwd() + root;
 
-  getAllFiles(directoryPath, [ignoreFile])
-    .then((files) => {
-      logger.logInfo({ 'Number of scanned files': files.length });
-      files.forEach((filename) => {
-        calculate.resolveAndUpdatePaths(filename, directoryPath, !!dryRun);
-      });
-    })
-    .catch(error => logger.logError({
-      description: `Unable to scan directory: ${directoryPath}`,
-      error,
-    }));
-}
+getAllFiles(directoryPath, [ignoreFile])
+  .then((files) => {
+    if (dry) {
+      logger.logInfo('Results of test(dry) run:');
+    }
+    logger.logInfo({ 'Number of scanned files': files.length });
+    files.forEach((filename) => {
+      calculate.resolveAndUpdatePaths(filename, directoryPath, !!dry);
+    });
+  })
+  .catch(error => logger.logError({
+    description: `Unable to scan directory: ${directoryPath}`,
+    error,
+  }));
